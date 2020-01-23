1f079a431658147c69c1edd4ccd82319a240bc04
Alina.Altanets <Alina.Altanets@mckesson.com>
2020-01-23 20:01:36 +0200

    Исправлена формула для вывода букв, соответствующих отрицательным числам в FiguresToLetters

--- src/ProgrammingTasks/FiguresToLetters.java
+++ src/ProgrammingTasks/FiguresToLetters.java
@@ -1,9 +1,10 @@
 package ProgrammingTasks;
 import java.util.Scanner;
+import static java.lang.Math.abs;
 
 /* TASK:
     Есть ряд, где цифре 43 соответствует буква А, 44 - В, 45 - С и т.д. Этот ряд продолжается как в большую, так и в меньшую стороны.
-    Нужно ввести любую цифру от -10^9 до + 10^9, и в ответ должна вывестись соответствующая ей буква.
+    Нужно ввести любую цифру, и в ответ должна быть выведена соответствующая ей буква.
 */
 
 public class FiguresToLetters {
@@ -23,9 +24,19 @@ public class FiguresToLetters {
     }
 
     private static void getLetter(long num) {
-        long x = (35 + num) % 26;
-        long litera = x + 65;
-        System.out.println("The matching letter is - " + (char) litera);
+
+        if (num>=-35) {
+            long x = (35 + num) % 26;
+            long litera = x + 65;
+            System.out.println("ID " + litera);
+            System.out.println("The matching letter is - " + (char) litera);
+        } else {
+            long x = (16 + abs(num)) % 26;
+            long litera = 90 - x;
+            System.out.println("ID " + litera);
+            System.out.println("The matching letter is - " + (char) litera);
+        }
+
     }
 
 }
